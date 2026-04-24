fb1997ca6755af21b3e9f5e5d4dd4d3642c22892
ricogm-source <ricardoggmenchu@gmail.com>
2026-04-24 08:17:45 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio progra 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_progra_3
+{
+    internal class Program
+    {
+        class Prestamo
+
+        {
+            private int codigo;
+
+
+
+                
+
+
+
+
+
+
+        }
+
+        class Estudiante
+
+        {
+
+
+
+        }
+            
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
